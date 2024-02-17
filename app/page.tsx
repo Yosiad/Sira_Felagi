6193df127af69380eae2cad0f328cd320eb62cb8
Yosef Aweke <106531110+Yosiad@users.noreply.github.com>
2024-02-17 11:44:41 +0300

    The Hero section is created and prisma and railway setup finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-
+import Hero from "./shared/Hero"
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Hero />
+    </>
   )
 }
 
---
+++ app/shared/Hero.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div id="home"
+    className="bg-[url('/hero.png')] h-screen relative w-full bg-cover mt-[-70px]">
+        <div className="flex flex-col h-full items-center justify-center pt-[82px] gap-20 w-[90%] mx-auto max-w-[1450px]">
+        <div className="text-center flex flex-col gap-3">
+          <h1 className="text-6xl font-extrabold text-black">
+            Land The{" "}
+            <span className="text-purple-600 font-extrabold">
+              Dream Job
+            </span>
+            , You Deserve
+          </h1>
+          <span className="font-thin">
+            100,000 jobs listed here! Your dream job is
+            waiting
+          </span>
+        </div>
+        {/* <SearchForm jobs={jobs} /> */}
+      </div>
+    </div>
+  )
+}
+
+export default Hero
